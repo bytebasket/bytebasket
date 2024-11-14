cec09e0ccebae400d837a7d65c5b614a2086837a
Hsiao Man Lin <irene891019@gmail.com>
2024-11-14 17:26:09 -0600

    add sign up page, make nav bar to all page

--- app/frontend/src/app/signup/page.tsx
+++ app/frontend/src/app/signup/page.tsx
@@ -11,19 +11,37 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function Login() {
+export default function Signup() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardTitle className="text-2xl">Create Account</CardTitle>
         <CardDescription>
           Enter your email and password to access your account.
         </CardDescription>
       </CardHeader>
       <CardContent>
+      <div className="text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/login" className="underline">
+          Login
+          </Link>
+        </div>
+        <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="fname">First Name*</Label>
+            </div>
+            <Input id="fname" type="text" required />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="lname">Last Name*</Label>
+            </div>
+            <Input id="lname" type="text" required />
+          </div>
         <div className="grid gap-4">
           <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
+            <Label htmlFor="email">Email*</Label>
             <Input
               id="email"
               type="email"
@@ -33,26 +51,24 @@ export default function Login() {
           </div>
           <div className="grid gap-2">
             <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
+              <Label htmlFor="password">Password*</Label>
             </div>
             <Input id="password" type="password" required />
           </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Confirm Password*</Label>
+            </div>
+            <Input id="confirmpassword" type="password" required />
+          </div>
           <Button type="submit" className="w-full">
-            Login
+            Sign Up
           </Button>
           <Button variant="outline" className="w-full">
-            Login with Google
+          Sign Up with Google
           </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="/signup" className="underline">
-            Sign up
-          </Link>
-        </div>
+        
       </CardContent>
     </Card>
   );
