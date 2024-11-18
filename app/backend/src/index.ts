07663d51c81d50db7b3a336a542ee9b0ce42705e
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2024-11-17 20:41:53 -0600

    chore: remove old routes

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -2,15 +2,11 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { usersRoute } from "./routes/users";
-import { shoppingListSubRoutes } from "./routes/shoppingList";
 
 const app = new Hono();
 app.use(logger());
 
-const apiRoutes = app
-  .basePath("/api")
-  .route("/users", usersRoute)
-  .route("/users/shopping-list", shoppingListSubRoutes);
+const apiRoutes = app.basePath("/api").route("/users", usersRoute);
 
 const port = 3001;
 console.log(`Server is running on http://localhost:${port}`);
